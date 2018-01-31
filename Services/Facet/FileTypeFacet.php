d8871c30863d95cbfc73207cc9f23ae2fdf99e1f
Florian Meyer <florian.meyer@project-engineers.de>
2018-01-31 09:36:16 +0100

    Added FileType Facets

---
+++ Services/Facet/FileTypeFacet.php
@@ -0,0 +1,36 @@
+<?php
+
+/*
+ * This file is part of the Stinger Entity Search package.
+ *
+ * (c) Oliver Kotte <oliver.kotte@stinger-soft.net>
+ * (c) Florian Meyer <florian.meyer@stinger-soft.net>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace StingerSoft\EntitySearchBundle\Services\Facet;
+
+use StingerSoft\EntitySearchBundle\Model\Document;
+
+class FileTypeFacet implements FacetServiceInterface {
+
+	const SERVICE_ID = 'stinger_soft_entity_search.facets.filetype';
+
+	public function getField() {
+		return Document::FIELD_CONTENT_TYPE;
+	}
+
+	public function getFormOptions() {
+		return array(
+			'label'              => 'stinger_soft_entity_search.forms.query.filetype.label',
+			'translation_domain' => 'StingerSoftEntitySearchBundle'
+		);
+	}
+
+	public function getFacetFormatter() {
+		return null;
+	}
+
+}
