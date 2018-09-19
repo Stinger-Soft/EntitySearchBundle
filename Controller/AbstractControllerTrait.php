862a3ef3389ddd60f9269f663e2262676327e041
Florian Meyer <florian.meyer@project-engineers.de>
2018-09-19 11:29:26 +0200

    Symfony 4 compatible controller trait

--- Controller/AbstractControllerTrait.php
+++ Controller/AbstractControllerTrait.php
@@ -1,9 +1,13 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: FlorianMeyer
- * Date: 18.01.2018
- * Time: 13:09
+
+/*
+ * This file is part of the Stinger Entity Search package.
+ *
+ * (c) Oliver Kotte <oliver.kotte@stinger-soft.net>
+ * (c) Florian Meyer <florian.meyer@stinger-soft.net>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
  */
 
 namespace StingerSoft\EntitySearchBundle\Controller;
@@ -15,35 +19,34 @@ trait AbstractControllerTrait {
 	/**
 	 * Returns a RedirectResponse to the given route with the given parameters.
 	 *
-	 * @param string $route      The name of the route
-	 * @param array  $parameters An array of parameters
-	 * @param int    $status     The status code to use for the Response
+	 * @param string $route The name of the route
+	 * @param array $parameters An array of parameters
+	 * @param int $status The status code to use for the Response
 	 *
 	 * @return RedirectResponse
 	 */
-	protected abstract function redirectToRoute($route, array $parameters = array(), $status = 302);
-
+	protected abstract function redirectToRoute(string $route, array $parameters = array(), int $status = 302): RedirectResponse;
 
 	/**
 	 * Creates and returns a Form instance from the type of the form.
 	 *
-	 * @param string $type    The fully qualified class name of the form type
-	 * @param mixed  $data    The initial data for the form
-	 * @param array  $options Options for the form
+	 * @param string $type The fully qualified class name of the form type
+	 * @param mixed $data The initial data for the form
+	 * @param array $options Options for the form
 	 *
 	 * @return FormInterface
 	 */
-	protected abstract function createForm($type, $data = null, array $options = array());
+	protected abstract function createForm(string $type, $data = null, array $options = array()): FormInterface;
 
 	/**
 	 * Renders a view.
 	 *
-	 * @param string   $view       The view name
-	 * @param array    $parameters An array of parameters to pass to the view
-	 * @param Response $response   A response instance
+	 * @param string $view The view name
+	 * @param array $parameters An array of parameters to pass to the view
+	 * @param Response $response A response instance
 	 *
 	 * @return Response A Response instance
 	 */
-	protected abstract function render($view, array $parameters = array(), Response $response = null);
+	protected abstract function render($view, array $parameters = array(), ?Response $response = null): Response;
 
 }
